5e2729801d90995fadd857d78ae4757257030547
Benjamin Frost <bfrost.schule@gmail.com>
2022-05-25 11:58:34 +0200

    feat: get all required user inputs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use std::io;
+use std::path::Path;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let config_path = get_config_path();
+    let desired_locale = get_desired_locale();
+    let desired_region = get_desired_region();
+
+    println!(
+        "Path: {}\nLocale: {}\nRegion: {}",
+        config_path, desired_locale, desired_region
+    );
+}
+
+fn get_config_path() -> String {
+    let mut input = String::new();
+    println!("Enter your config path:");
+    io::stdin()
+        .read_line(&mut input)
+        .ok()
+        .expect("Failed to read line");
+    let exists = Path::new(&input.trim()).exists();
+    if !exists {
+        println!("Path does not exist");
+        process::exit(1);
+    }
+    input
+}
+
+fn get_desired_locale() -> String {
+    let mut input = String::new();
+    println!("Enter your desired locale:");
+    io::stdin()
+        .read_line(&mut input)
+        .ok()
+        .expect("Failed to read line");
+    input
+}
+
+fn get_desired_region() -> String {
+    let mut input = String::new();
+    println!("Enter your desired region:");
+    io::stdin()
+        .read_line(&mut input)
+        .ok()
+        .expect("Failed to read line");
+    input
 }
